ffff46ffc0d5c87c96d951dd0c719afad870e9cc
darile <darilecodeur@gmail.com>
2022-12-24 22:44:12 +0100

    edition 2

--- takprojet/settings.py
+++ takprojet/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['151.80.61.88', 'tak-media.tech', 'www.tak-media.tech' '127.0.0.1']
 
 
 # Application definition
