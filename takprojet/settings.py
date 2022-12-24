bbb2a5fe384ccd3398ac8db90ad8285c4b61bae9
darile <darilecodeur@gmail.com>
2022-12-24 22:37:31 +0100

    edition

--- takprojet/settings.py
+++ takprojet/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['151.80.61.88', '.tak-media.tech', 'www.tak-media.tech' '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 ''' 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
